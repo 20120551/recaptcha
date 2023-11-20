6cc9294e995df1c1a25b27834dc64e6262c3d301
An Le <anle@BZ-MAC-015.local>
2023-11-20 14:32:28 +0700

    show more details related to assesment

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
-const { create } = require('express-handlebars');
-const bodyParser = require('body-parser');
-const path = require('path');
+const { create } = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
 const { createAssessment } = require("./recaptchaHandler");
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 const hbs = create({
-    extname: 'hbs'
+  extname: "hbs",
 });
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("form");
-})
+  res.render("form");
+});
 app.post("/recaptcha/v3", async (req, res) => {
-    console.log(req.body);
-    const { token, action } = req.body;
-    const data = await createAssessment({ token, recaptchaAction: action });
-    res.render("form", { score: data || 0 });
-})
+  console.log(req.body);
+  const { token, action } = req.body;
+  const assessment = await createAssessment({
+    token,
+    recaptchaAction: action,
+  });
+  res.render("form", {
+    score: assessment.riskAnalysis.score || 0,
+    assessment: JSON.stringify(assessment),
+  });
+});
 app.listen(3000, () => {
-    console.log("server is listening in port 3000");
-})
+  console.log("server is listening in port 3000");
+});
